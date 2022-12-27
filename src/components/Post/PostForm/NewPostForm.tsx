bb0908e020a165a89ad1516c2dd552d08c4ae0c1
gapur <kaloycant@gmail.com>
2022-12-27 18:56:15 +0100

    #4. TextInputs and ImageUpload done

--- src/components/Post/PostForm/NewPostForm.tsx
+++ src/components/Post/PostForm/NewPostForm.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Flex, Icon } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 import { BiPoll } from "react-icons/bi";
 import { BsLink45Deg, BsMic } from "react-icons/bs";
 import { IoDocumentText, IoImageOutline } from "react-icons/io5";
@@ -7,8 +8,10 @@ import { AiFillCloseCircle } from "react-icons/ai";
 import { useRecoilState, useSetRecoilState } from "recoil";
 import { firestore, storage } from "../../../firebase/clientApp";
 import TabItem from './TabItem';
+import TextInputs from "./TextInputs";
+import ImageUpload from "./ImageUpload";
 
-const formTabs = [
+const formTabs: TabItemType[] = [
   {
     title: "Post",
     icon: IoDocumentText,
@@ -42,6 +45,43 @@ type NewPostFormProps = {
 
 const NewPostForm:React.FC<NewPostFormProps> = () => {
   const [selectedTab, setSelectedTab] = useState(formTabs[0].title);
+  const [textInputs, setTextInputs] = useState({
+    title: "",
+    body: ""
+  });
+  const [selectedFile, setSelectedFile] = useState<string>("");
+  const selectFileRef = useRef<HTMLInputElement>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const handleCreatePost = async () => {
+
+  };
+
+  const onSelectImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const reader = new FileReader();
+    if (event.target.files?.[0]) {
+      reader.readAsDataURL(event.target.files[0]);
+    }
+
+    reader.onload = (readerEvent) => {
+      if (readerEvent.target?.result) {
+        setSelectedFile(readerEvent.target?.result as string);
+      }
+    };
+  };
+
+  const onTextChange = ({
+    target: { name, value },
+  }: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setTextInputs((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+
   return (
     <Flex direction="column" bg="white" borderRadius={4} mt={2}>
       <Flex width="100%">
@@ -53,6 +93,25 @@ const NewPostForm:React.FC<NewPostFormProps> = () => {
             setSelectedTab={setSelectedTab}
           />
         ))}
+      </Flex>
+      <Flex p={4}>
+        {selectedTab === "Post" && (
+          <TextInputs
+            textInputs={textInputs}
+            handleCreatePost={handleCreatePost}
+            onChange={onTextChange}
+            loading={loading}
+          />
+        )}
+        {selectedTab === "Images & Video" && (
+          <ImageUpload
+            selectedFile={selectedFile}
+            setSelectedFile={setSelectedFile}
+            setSelectedTab={setSelectedTab}
+            selectFileRef={selectFileRef}
+            onSelectImage={onSelectImage}
+          />
+        )}
       </Flex>
     </Flex>
   )
